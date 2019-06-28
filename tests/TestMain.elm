58dfaab47372a0f48a966a5fd5e07ccb6b962608
Ian Denhardt <ian@zenhack.net>
2019-06-28 18:13:41 -0400

    Smarter approach to normalization
    rather than fussing with what the decoding library spits out, just
    normalize both.
    
    This may fix the tests, but they take a very long time to run, and I
    have to get out the door. Will check again later. Note that I tried
    changing the infinite loop to a debug.todo and it was still taking
    forever, so it's not that.

--- tests/TestMain.elm
+++ tests/TestMain.elm
@@ -13,6 +13,29 @@ import Fuzz exposing (Fuzzer)
 import Test exposing (..)
 
 
+{-| "Normalize" the csv. This is needed because some values encode to the
+same output, so the test that expects that decode and encode are inverses
+would otherwise fail. Instead of testing against the input, we test against
+a normalized input.
+-}
+normalizeCsv : E.Csv -> E.Csv
+normalizeCsv { headers, records } =
+    let
+        -- It's impossible to tell the difference between [] and [""] when
+        -- encoded, so we normalize former before comparison.
+        removeEmpties xs =
+            case xs of
+                [ "" ] ->
+                    []
+
+                _ ->
+                    xs
+    in
+    { headers = removeEmpties headers
+    , records = List.map removeEmpties records
+    }
+
+
 csvFuzzer : Fuzzer E.Csv
 csvFuzzer =
     Fuzz.map2 E.Csv
@@ -26,6 +49,8 @@ suite =
         "Encode-then-decode should do nothing."
         (\csv ->
             Expect.equal
-                (Ok csv)
-                (Csv.parse (E.toString csv))
+                (Ok (normalizeCsv csv))
+                (Csv.parse (E.toString csv)
+                    |> Result.map normalizeCsv
+                )
         )
