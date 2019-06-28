ff2b97a954cc4a75237309478f3949e6825661a6
Ian Denhardt <ian@zenhack.net>
2019-06-28 16:47:32 -0400

    Add a 'toString', write a fuzz test.
    The test is currently failing (found some edge cases); need to fix.

--- src/Csv/Encode.elm
+++ src/Csv/Encode.elm
@@ -1,17 +1,18 @@
-module Csv.Encode exposing (Csv, toBytes, toEncoder)
+module Csv.Encode exposing (Csv, toBytes, toEncoder, toString)
 
 {-| This module provides support for rendering data in csv (comma separateed
 values) format. The format emitted is as desribed in [RFC4180][1].
 
 If you want to _parse_ csv files, look at the package `lavosa/elm-csv`.
 
-@doc toBytes, toEncoder
+@doc toString, toBytes, toEncoder
 
 [1]: https://tools.ietf.org/html/rfc4180
 
 -}
 
 import Bytes exposing (Bytes)
+import Bytes.Decode as D
 import Bytes.Encode as E
 
 
@@ -36,6 +37,25 @@ toEncoder { headers, records } =
 toBytes : Csv -> Bytes
 toBytes =
     toEncoder >> E.encode
+
+
+{-| Convert a `Csv` to a string.
+-}
+toString : Csv -> String
+toString csv =
+    let
+        bytes =
+            toBytes csv
+    in
+    case D.decode (D.string (Bytes.width bytes)) bytes of
+        Just v ->
+            v
+
+        Nothing ->
+            -- This is impossible, but we have no way of convincing the
+            -- type checker of this. So we just infinite loop in this
+            -- case:
+            toString csv
 
 
 formatLines : List (List String) -> E.Encoder
---
+++ tests/TestMain.elm
@@ -0,0 +1,31 @@
+module TestMain exposing (suite)
+
+{-| Tests for the csv encoder.
+
+We test against `lavosa/elm-csv` for decoding.
+
+-}
+
+import Csv
+import Csv.Encode as E
+import Expect
+import Fuzz exposing (Fuzzer)
+import Test exposing (..)
+
+
+csvFuzzer : Fuzzer E.Csv
+csvFuzzer =
+    Fuzz.map2 E.Csv
+        (Fuzz.list Fuzz.string)
+        (Fuzz.list (Fuzz.list Fuzz.string))
+
+
+suite : Test
+suite =
+    fuzz csvFuzzer
+        "Encode-then-decode should do nothing."
+        (\csv ->
+            Expect.equal
+                csv
+                (Csv.parse (E.toString csv))
+        )
